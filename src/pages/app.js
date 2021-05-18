ef6487021ddfb17afb12c8e7a4105a32d13b0d08
Dominick <d3lee@ucsd.edu>
2021-05-18 00:41:39 -0700

    Day 1

---
+++ src/pages/app.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import "../style/app.scss"
+
+const AppPage = () => {
+    return (
+      <main>
+        <title>Home Page</title>
+        <h1>App</h1>
+        
+      </main>
+    )
+  }
+  
+  export default AppPage
